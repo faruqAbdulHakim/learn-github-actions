7120a9b4a5d4f3976b10a87ba0d942de68551d02
faruqAbdulHakim <faruqabdulhakim2001@gmail.com>
2022-10-06 20:53:41 +0700

    test fail

--- app.test.js
+++ app.test.js
@@ -3,3 +3,7 @@ const { add } = require('./app');
 it('add 1 and 2 Should return 3', () => {
   expect(add(1, 2)).toBe(3);
 });
+
+it('add 3 and 2 Should return 5', () => {
+  expect(add(1, 2)).toBe(6);
+});
